356045fac90f0f9da730eb2f76f19cf705268861
FJ <battersman27@gmail.com>
2019-11-14 09:11:14 +0300

    ++

--- app/blocks/slider/slider.js
+++ app/blocks/slider/slider.js
@@ -2,21 +2,20 @@ $('[data-js=fancybox]').fancybox();
 $('[data-js=slider]').slick({
   rows: false,
   swipeToSlide: true,
-  slidesToScroll: 1,
+  // centerMode: true,
   slidesToShow: 3,
   dots: true,
   arrows: true,
   responsive: [{
     breakpoint: 960,
     settings: {
-      variableWidth: false,
       slidesToShow: 1,
       adaptiveHeight: true
     }
   }]
 });
-$('.audio').each(function(){
 
+$('.audio').each(function(){
   this.addEventListener('loadedmetadata', function() {
     var _this = this.duration;
     var minutes = Math.floor(_this / 60) + ' ';
